c147dfa357425519b299f68c426b4554c8a6e001
Yjet e Gjakoves <yjetegjakoves@gmail.com>
2023-12-21 18:25:03 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
-//gjin stublla
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from "./Login";
+import Signup from "./Signup";
+import Home from './Home';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      
+      <Routes>
+
+          <Route path = '/' element = {<Login />}></Route>
+          <Route path = '/signup' element = {<Signup />}></Route>
+          <Route path = '/home' element = {<Home />}></Route>
+
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
